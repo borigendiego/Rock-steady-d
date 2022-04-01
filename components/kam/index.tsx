4170e56bb00e003a7b7c419d71c78fd98def513c
Diego Borigen <diego@iddigital.com.au>
2022-04-01 08:33:05 -0300

    Add LIFE text + fix form style

--- components/kam/index.tsx
+++ components/kam/index.tsx
@@ -5,16 +5,22 @@ import styles from './kam.module.scss'
 
 const Kam = () => {
     return(
-        <div className={`flex sm:flex-row h-screen sm:justify-around items-center justify-center`} id={'ABOUT'}>
+        <div className={`flex flex-wrap flex-col md:flex-row sm:justify-around items-center justify-center`} id={'ABOUT'}>
             <Box
                 image='/assets/images/kam-home.png'
                 title='KAMLESH LAD - FOUNDER'
                 text='I can help you reach your financial goals so
                     you can focus on the more important things in life.'
              />
-             <div className={`${styles.title_container} sm:flex hidden flex-col w-11/12 sm:w-fit justify-center items-center`}>
-                <h1 className={`text-center text-white`}>DIFFERENT</h1>
-                <h1 className={`text-center text-white empty-font`}>FROM THE REST</h1>
+             <div className={'flex flex-wrap flex-col'}>
+                <div className={`${styles.title_container} sm:flex flex-col justify-center items-center`}>
+                    <h1 className={`text-center text-white text clamp-h1`}>DIFFERENT</h1>
+                    <h1 className={`text-center text-white empty-font clamp-h1`}>FROM THE REST</h1>
+                </div>
+                <span className={'mt-10 flex flex-col text-center'}>
+                    <h3>Create Solutions to Transform L.I.F.E.</h3>
+                    <h3>Living In Freedom Everyday</h3>
+                </span>
              </div>
         </div>
     )

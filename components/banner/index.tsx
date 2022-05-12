f76873e9cf616b308e4d33b952395557fc08819a
Diego Borigen <diego@iddigital.com.au>
2022-05-12 11:11:25 -0300

    Add overflow hidden on banner

--- components/banner/index.tsx
+++ components/banner/index.tsx
@@ -4,7 +4,7 @@ import Header from '../header';
 
 const Banner = () => {
     return(
-        <div className={`flex flex-col sm:justify-start sm:px-12 sm:pt-2 sm:max-h-screen h-screen ${styles.background}`}>
+        <div className={`flex flex-col sm:justify-start sm:px-12 sm:pt-2 sm:max-h-screen h-screen ${styles.background} overflow-hidden`}>
             <Header />
             <img src={'/assets/images/solutions_letras.png'} className={'w-full sm:m-auto sm:relative sm:bottom-12 sm:h-auto h-96 mt-12'} />
         </div>

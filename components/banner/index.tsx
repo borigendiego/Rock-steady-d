d1291366a9bcaceaf5ba1c95fb82044c45cc5d6c
Diego Borigen <diego@iddigital.com.au>
2022-05-19 09:12:02 -0300

    Fix banner text styles

--- components/banner/index.tsx
+++ components/banner/index.tsx
@@ -6,15 +6,15 @@ const Banner = () => {
     return(
         <div className={`flex flex-col sm:justify-start sm:px-12 sm:pt-2 sm:max-h-screen h-screen ${styles.background} overflow-hidden`}>
             <Header />
-            <div className={'flex flex-col items-center margin-auto flex-grow justify-center'}>
-                <span className={'flex'}>
-                    <img src={'/assets/images/star_2.png'} className={'object-contain w-16 mr-7'} />
-                    <h3 className={'text-center empty-font text-[2rem] opacity-[0.7]'}>OUR CREATIVE</h3>
-                    <img src={'/assets/images/star_2.png'} className={'object-contain w-16 ml-7 rotate-180'} />
+            <div className={'flex flex-col items-center flex-grow justify-center w-[32rem] m-auto'}>
+                <span className={'flex w-full justify-between'}>
+                    <img src={'/assets/images/star_2.png'} className={'object-contain w-16'} />
+                    <h3 className={'text-center empty-font text-[2rem]  '}>OUR CREATIVE</h3>
+                    <img src={'/assets/images/star_2.png'} className={'object-contain w-16 rotate-180'} />
                 </span>
-                <h1 className={'text-center empty-font text-[4.7rem] opacity-[0.7]'}>SOLUTIONS</h1>
-                <h1 className={'text-center text-[10rem] opacity-[0.7]'}>ROCK</h1>
-                <h3 className={'text-center text-[1rem] bg-[#ffffffa8] text-black rounded-lg py-1 px-6'}>
+                <h1 className={'text-center empty-font text-[4.8rem] banner-letters'}>SOLUTIONS</h1>
+                <h1 className={'text-center text-[10.4rem] opacity-[0.7] tracking-[-10px] banner-letters -mt-10 -ml-3 -mb-5'}>ROCK</h1>
+                <h3 className={'text-center text-[1rem] bg-[#ffffffa8] text-black rounded-xl py-1 px-6 w-full'}>
                     Digital Consulting •  Blockchain • Metaverse • DeFi
                 </h3>
                 <img src={'/assets/images/star_3_crop.png'} className={'mt-6 w-20 h-8 object-fill'} />
